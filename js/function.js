6fa1311dd325c6f9fc1a0d6ef386b9af5bdabe61
Dmitrii Timofeev <28dngn@gmail.ru>
2023-06-10 04:29:37 +0300

    добавлена домашка-2

---
+++ js/function.js
@@ -0,0 +1,77 @@
+// Task_1 - Проверка длины строки
+
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+checkStringLength('Слоны идут на север', 19);
+
+
+// Task_2_1 - Проверка на палиндром
+
+const isPalindromeOne = (string) => string === string.split('').reverse('').join('');
+
+isPalindromeOne('топот');
+
+
+// Task_2_2 - Проверка на палиндром строки с пробелами
+
+function isPalindromeTwo (string) {
+  const normalString = string.replaceAll(' ', '').toLowerCase();
+  const newString = normalString.split('').reverse('').join('');
+
+  return normalString === newString;
+}
+
+isPalindromeTwo ('Лёша на полке клопа нашёл ');
+
+// Упоротый вариант
+
+const isPalindromeBad = (string) => string.replaceAll(' ', '').toLowerCase() === string.replaceAll(' ', '').toLowerCase().split('').reverse('').join('');
+
+isPalindromeBad('Лёша на полке клопа нашёл ');
+
+
+// Task_3_1 - Дополнительное (извлечение цифр из строки)
+
+function getNumberOne (string) {
+  let newString = '';
+
+  for (const elem of string) {
+    if (!Number.isNaN(parseInt(elem, 10))) {
+      newString += elem;
+    }
+  }
+
+  if (newString) {
+    return +newString;
+  }
+
+  return NaN;
+}
+
+getNumberOne('1 кефир, 0.5 батона');
+
+
+// Task_3_2 - Дополнительное (извлечение цифр из строки), если пришло число
+
+function getNumberTwo (string) {
+  let newString = '';
+
+  // проверка на тип данных - число
+  if (typeof(string) === 'number') {
+    string = Math.abs(string).toString();
+  }
+
+  for (const elem of string) {
+    if (!Number.isNaN(parseInt(elem, 10))) {
+      newString += elem;
+    }
+  }
+
+  if (newString) {
+    return +newString;
+  }
+
+  return NaN;
+}
+
+getNumberTwo('-1.5');
