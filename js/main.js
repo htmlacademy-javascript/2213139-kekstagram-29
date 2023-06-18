6806d81e5286aba2520c6a54c13cca6fd56b4766
Dmitrii Timofeev <28dngn@gmail.ru>
2023-06-18 18:40:29 +0300

    домашка_4 окончательные исправения

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-// ДМИТРИЙ ТИМОФЕЕВ
+// Количество описаний фото
+
+const NUMBER_DESCRIPTIONS_FOTO = 25;
 
 // Фразы для описания фотографий
 
-const DESCRIPTIONS = {1: 'Отель с бассейном', 2: 'Указатель', 3: 'Море... Пляж...', 4: 'Вот такие мы красивые!', 5: 'Весёлая кухня', 6: 'Шикарное авто', 7: 'Свежее и вкусное', 8: 'Вкуснотища!', 9: 'Самолёт', 10: 'Хранение обуви', 11: 'Дорога к морю', 12: 'Авто в аренду', 13: 'Салат с красной рыбой', 14: 'Суши с котом', 15: 'Стильные валенки', 16: 'Самолёт над облаками', 17: 'Хор', 18: 'Раритетное авто', 19: 'Тапочки с освещением', 20: 'Пальмы', 21: 'вкусноеёшее блюдо', 22: 'Морский закат', 23: 'Краб на камне', 24: 'Супер-шоу', 25: 'сафари на авто',};
+const DESCRIPTIONS = ['Отель с бассейном', 'Указатель', 'Море... Пляж...', 'Вот такие мы красивые!', 'Весёлая кухня', 'Шикарное авто', 'Свежее и вкусное', 'Вкуснотища!', 'Самолёт', 'Хранение обуви', 'Дорога к морю', 'Авто в аренду', 'Салат с красной рыбой', 'Суши с котом', 'Стильные валенки', 'Самолёт над облаками', 'Хор', 'Раритетное авто', 'Тапочки с освещением', 'Пальмы', 'Вкуснеёшее блюдо', 'Морский закат', 'Краб на камне', 'Супер-шоу', 'Сафари на авто'];
 
 // Имена для авторов комментариев
 
@@ -18,73 +20,59 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-// Количество описаний фото
+const AVATAR_COUNTER = {
+  min: 1,
+  max: 6
+};
 
-const NUMBER_DESCRIPTIONS_FOTO = 25;
+const LIKES_COUNTER = {
+  min: 15,
+  max: 200
+};
+
+const COMMENTS_COUNTER = {
+  min: 1,
+  max: 30
+};
 
 // Получение случайного числа из диапазона
 
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-  return Math.floor(result);
-};
+// Получение случайного элемента из массива
 
-// Получение ID для фото по порядку
+const getArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
-const createIdGenerator = () => {
-  let lastGeneratedId = 1;
+// Получение одного или двух комментариев
 
-  return () => lastGeneratedId++;
-};
-
-const createRandomId = (min, max) => {
-  const previousValues = [];
-
-  return () => {
-    let currentValue = getRandomInteger(min, max);
-
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-
-    previousValues.push(currentValue);
-    return currentValue;
-  };
+const createMessage = () => {
+  const messages = Array.from({length: getRandomInteger(1, 2)}, () => getArrayElement(MESSAGES));
+  return Array.from(new Set(messages)).join(' ');
 };
 
 // Создание объекта для комментариев
 
-const getRandomId = createRandomId(1, 1000);
-
-const createCommentFoto = () => ({
-  id: getRandomId(),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: `${MESSAGES[getRandomInteger(0, MESSAGES.length - 1)]}`,
-  name: `${NAMES[getRandomInteger(0, NAMES.length - 1)]}`,
+const createPhotoComment = () => ({
+  id: getRandomInteger(1, 1000),
+  avatar: `img/avatar-${getRandomInteger(AVATAR_COUNTER.min, AVATAR_COUNTER.max)}.svg`,
+  message: createMessage(),
+  name: getArrayElement(NAMES),
 });
 
 // Создание объекта для описания фотографии
 
-const generatePhotoId = createIdGenerator();
-const generatePhotoUrl = createIdGenerator();
-const generateDescription = createIdGenerator();
+let commentId = 1;
+let descriptionId = 1;
 
-const createDescriptionFoto = () => ({
-  id: generatePhotoId(),
-  url: `photos/${generatePhotoUrl()}.jpg`,
-  description: `${DESCRIPTIONS[generateDescription()]}`,
-  likes: getRandomInteger(15, 200),
-  comments: Array.from({length: getRandomInteger(0, 30)}, createCommentFoto),
+const createPhotoDescription = () => ({
+  id: commentId,
+  url: `photos/${commentId++}.jpg`,
+  description: DESCRIPTIONS[(descriptionId++) - 1],
+  likes: getRandomInteger(LIKES_COUNTER.min, LIKES_COUNTER.max),
+  comments: Array.from({length: getRandomInteger(COMMENTS_COUNTER.min, COMMENTS_COUNTER.max)}, createPhotoComment),
 });
 
-const fotoDescriptions = Array.from({length: NUMBER_DESCRIPTIONS_FOTO}, createDescriptionFoto);
+const createPhotoDescriptions = () => Array.from({length: NUMBER_DESCRIPTIONS_FOTO}, createPhotoDescription);
 
 // eslint-disable-next-line no-console
-console.log(fotoDescriptions);
+console.log(createPhotoDescriptions());
