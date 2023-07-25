67d53be11eb68564b84fe9070989eb9366a63f91
Keks <keks@htmlacademy.ru>
2023-07-26 00:57:38 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/thumbnails/init-thumbnails.js
@@ -0,0 +1,21 @@
+import {renderThumbnails} from './render-thumbnails.js';
+import {getData} from '../utils/api.js';
+import {showMessage} from '../utils/messages.js';
+import {initFilter, getFilteringData} from './filter.js';
+
+const GET_URL = 'https://29.javascript.pages.academy/kekstagram/data';
+const ERROR_TEXT = 'Ошибка загрузки данных';
+const STATE = 'error';
+
+const currentId = document.querySelector('.img-filters__button--active').id;
+
+const getSuccess = (data) => {
+  initFilter(data);
+  renderThumbnails(getFilteringData(currentId, data));
+};
+
+const getError = () => showMessage(STATE, ERROR_TEXT);
+
+const initThumbnails = () => getData(GET_URL, getSuccess, getError);
+
+export {initThumbnails};
